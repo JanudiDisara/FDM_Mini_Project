d8ea022c89fb898e98a34d51fd2389eeada7af73
sanjay-dev-ds <sanjay28.js@gmail.com>
2022-10-31 22:41:43 +0530

    Last-Commit

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ def predict():
     elif model_name == "naive_bayes":
         model = pickle.load(open('models/naive_mdl.pkl', 'rb'))
     elif model_name == "random_forest":
-        model = pickle.load(open('models/rfc.pkl', 'rb'))
+        model = pickle.load(open('models/rfc_model.pkl', 'rb'))
     elif model_name == "support_vector_machine":
-        model = pickle.load(open('models/svm.pkl', 'rb'))
+        model = pickle.load(open('models/SVM_FDM.pkl', 'rb'))
     
 
-    features = [data["lead_time"], data["same_room"], data["cancelled"], data["special_requests"], data["booking_changes"]]
+    features = [data["lead_time"], data["same_room"], data["Previous_Cancellation"], data["special_requests"], data["booking_changes"]]
     final_features = [np.array([float(feature) for feature in features])]
 
     prediction = model.predict(final_features)
@@ -44,9 +44,9 @@ def predict():
     output = round(prediction[0])
 
     if output == 0:
-        result = "Reservation is Canceled"
+        result = "Reservation is Cancelled"
     else:
-        result = "Reservation is not Canceled"
+        result = "Reservation is not Cancelled"
 
     return render_template('index.html', prediction_text=f"{result}")
 
